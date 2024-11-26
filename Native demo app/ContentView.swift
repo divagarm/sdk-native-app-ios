fd1e6484649e705792d530061fbddf569cb39add
Divagar <divagar.m@rently.com>
2024-11-26 07:11:15 +0530

    Initial Commit

---
+++ Native demo app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Native demo app
+//
+//  Created by Divagar M on 26/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Native demo app/Native_demo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Native_demo_appApp.swift
+//  Native demo app
+//
+//  Created by Divagar M on 26/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Native_demo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
